1b7df58948a1dced07bab7fecfc3256633995a4d
Kyle Baron <kyleb@metrumrg.com>
2024-01-04 23:19:44 -0600

    re-build

--- global.R
+++ global.R
@@ -1,2 +1,2 @@
-knitr::opts_chunk$set(message = FALSE, warning = FALSE,comment='.',cache=TRUE,autodep=TRUE)
-options(mrgsolve.soloc = "mrgsolve_builds")
+knitr::opts_chunk$set(message = FALSE, warning = FALSE,comment='.')
+options(mrgsolve.soloc = "build")
